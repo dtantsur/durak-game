0ffcb362b9e7db392d397dda2be50bc62e9a38c8
Dmitry Tantsur <divius.inside@gmail.com>
2018-08-12 12:41:03 +0200

    Project skeleton

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+// Copyright 2018 Dmitry Tantsur <divius.inside@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Durak card game 2x2.
+//!
+//! See [wikipedia](https://en.wikipedia.org/wiki/Durak) for a game
+//! explanation. This package implements the simpest variant as a CLI
+//! application.
+
+// NOTE: we do not use generic deny(warnings) to avoid breakages with new
+// versions of the compiler. Add more warnings here as you discover them.
+// Taken from https://github.com/rust-unofficial/patterns/
+#![deny(const_err,
+        dead_code,
+        improper_ctypes,
+        legacy_directory_ownership,
+        missing_copy_implementations,
+        missing_debug_implementations,
+        non_shorthand_field_patterns,
+        no_mangle_generic_items,
+        overflowing_literals,
+        path_statements ,
+        patterns_in_fns_without_body,
+        plugin_as_library,
+        private_in_public,
+        private_no_mangle_fns,
+        private_no_mangle_statics,
+        safe_extern_statics,
+        trivial_casts,
+        trivial_numeric_casts,
+        unconditional_recursion,
+        unions_with_drop_fields,
+        unsafe_code,
+        unused,
+        unused_allocation,
+        unused_comparisons,
+        unused_doc_comments,
+        unused_extern_crates,
+        unused_import_braces,
+        unused_parens,
+        unused_qualifications,
+        unused_results,
+        while_true)]
+
+extern crate termion;
+
+mod game;
+mod ui;
+
+use std::io;
+
+use termion::raw::IntoRawMode;
+
+fn main() {
+    let stdin = io::stdin();
+    let stdout = io::stdout().into_raw_mode()
+        .expect("Cannot move stdout to raw mode");
+    let g = game::Game::new();
+    let mut u = ui::Ui::new(g, stdin, stdout);
+    u.start().expect("Game crashed");
+}
---
+++ src/ui.rs
@@ -0,0 +1,68 @@
+// Copyright 2018 Dmitry Tantsur <divius.inside@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Game UI.
+
+use std::fmt;
+use std::io;
+
+use termion::{clear, cursor};
+use termion::event::{Event, Key};
+use termion::input::{self, TermRead};
+
+use super::game::Game;
+
+
+pub struct Ui<R, W: io::Write> {
+    game: Game,
+    stdin: input::Events<R>,
+    stdout: input::MouseTerminal<W>,
+}
+
+
+const START: cursor::Goto = cursor::Goto(1, 2);
+
+impl<R: io::Read, W: io::Write> Ui<R, W> {
+    pub fn new(game: Game, stdin: R, stdout: W) -> Ui<R, W> {
+        Ui {
+            game: game,
+            stdin: stdin.events(),
+            stdout: stdout.into(),
+        }
+    }
+
+    pub fn start(&mut self) -> Result<(), io::Error> {
+        loop {
+            self.draw()?;
+
+            let cmd = self.stdin.next().unwrap()?;
+            match cmd {
+                Event::Key(Key::Char('q')) => return self.exit(),
+                _ => ()
+            }
+        }
+    }
+
+    fn draw(&mut self) -> Result<(), io::Error> {
+        write!(self.stdout, "{}{}Durak game, press q to exit{}{}",
+               clear::All, cursor::Goto(1, 1), START, self.game)?;
+        self.stdout.flush()?;
+
+        Ok(())
+    }
+
+    fn exit(&mut self) -> Result<(), io::Error> {
+        write!(self.stdout, "{}{}Bye", clear::All, cursor::Goto(1, 1))
+    }
+}
+
+impl fmt::Display for Game {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<placeholder>")
+    }
+}
